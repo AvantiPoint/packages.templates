f11a3096f4b4a3486122b9329be4da931b365746
Dan Siegel <me@dansiegel.net>
2023-04-24 11:21:23 -0600

    feat: update token generation strategy

--- templates/NuGetFeedTemplate/Data/Models/AuthToken.cs
+++ templates/NuGetFeedTemplate/Data/Models/AuthToken.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
 namespace NuGetFeedTemplate.Data.Models
 {
     public class AuthToken
     {
+        private const int TokenLength = 32;
+
         public AuthToken()
         {
-             Key = Guid.NewGuid().ToString().Replace("-", string.Empty);
+             Key = GenerateKey(TokenLength);
         }
 
-        [MaxLength(32)]
+        [MaxLength(TokenLength)]
         public string Key { get; set; }
 
         [MaxLength(60)]
@@ -33,6 +35,14 @@ namespace NuGetFeedTemplate.Data.Models
                 return false;
 
             return true;
+        }
+
+        private static string GenerateKey(int length)
+        {
+            var tokenBytes = RandomNumberGenerator.GetBytes(length);
+            var key = Convert.ToBase64String(tokenBytes);
+            key = Regex.Replace(key, @"[=]+$", string.Empty);
+            return key[..length];
         }
     }
 }
