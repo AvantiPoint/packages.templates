f0867e8a88b5d3c1c5426162589ae6ced8fbf0df
Dan Siegel <me@dansiegel.net>
2025-11-16 10:37:17 -0600

    Adds missing AuthTokens migration
    Introduces a new migration for AuthTokens,
    including a new NotificationTypeEnum column in
    the TokenNotifications table.
    
    Updates the foreign key relationship between
    TokenNotifications and AuthTokens.

---
+++ templates/NuGetFeedTemplate/Migrations/20251116163200_AuthTokens.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NuGetFeedTemplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TokenNotifications_AuthTokens_TokenKey",
+                table: "TokenNotifications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NotificationTypeEnum",
+                table: "TokenNotifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TokenNotifications_AuthTokens_TokenKey",
+                table: "TokenNotifications",
+                column: "TokenKey",
+                principalTable: "AuthTokens",
+                principalColumn: "Key");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TokenNotifications_AuthTokens_TokenKey",
+                table: "TokenNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "NotificationTypeEnum",
+                table: "TokenNotifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TokenNotifications_AuthTokens_TokenKey",
+                table: "TokenNotifications",
+                column: "TokenKey",
+                principalTable: "AuthTokens",
+                principalColumn: "Key",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
